61e2655f07ae4ac82c4d3c61eca7500b775b688c
樂門樂曼 <diana900710@gmail.com>
2023-06-06 17:07:33 +0800

    首頁復刻

--- CHU_PolicyPlatform_1/Controllers/HomeController.cs
+++ CHU_PolicyPlatform_1/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using CHU_PolicyPlatform_1.Models;
+﻿using CHU_PolicyPlatform_1.Data;
+using CHU_PolicyPlatform_1.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -6,21 +7,46 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using CHU_PolicyPlatform_1.ViewModels;
 
 namespace CHU_PolicyPlatform_1.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ProposeContext _prop;
 
-        public HomeController(ILogger<HomeController> logger)
+        
+        public HomeController(ProposeContext prop)
         {
-            _logger = logger;
+            _prop = prop;
         }
 
         public IActionResult Index()
         {
-            return View();
+            List<Proposal> proposals = new List<Proposal>
+            {
+                new Proposal {ProposalId = "P001", Title="aaa", CategoryId="C001"},
+                new Proposal {ProposalId = "P002", Title="bbb", CategoryId= "C004"},
+                new Proposal {ProposalId = "P003", Title="ccc", CategoryId = "C002"},
+                new Proposal {ProposalId = "P004", Title="ddd", CategoryId = "C002"},
+                new Proposal {ProposalId = "P005", Title="eee", CategoryId = "C003"},
+                new Proposal {ProposalId = "P006", Title="111", CategoryId = "C003"},
+                new Proposal {ProposalId = "P007", Title="222", CategoryId = "C001"},
+                new Proposal {ProposalId = "P008", Title="333", CategoryId = "C004"},
+                new Proposal {ProposalId = "P009", Title="444", CategoryId = "C001"},
+            };
+            var votes = _prop.Votes;
+            
+            ViewData["proposal"] = proposals;
+            ViewData["vote"] = votes;
+
+            ScanViewModel scanVM = new ScanViewModel()
+            {
+                Proposals = proposals,
+                Votes = votes.ToList(),
+            };
+
+            return View(scanVM);
         }
 
         public IActionResult Privacy()
---
+++ CHU_PolicyPlatform_1/ViewModels/ScanViewModel.cs
@@ -0,0 +1,11 @@
+﻿using CHU_PolicyPlatform_1.Models;
+using System.Collections.Generic;
+
+namespace CHU_PolicyPlatform_1.ViewModels
+{
+    public class ScanViewModel
+    {
+        public List<Proposal> Proposals {  get; set; }
+        public List<Vote> Votes { get; set; }
+    }
+}
