8093bc09232123ef0f45bc89cca745a717645b8e
樂門樂曼 <diana900710@gmail.com>
2023-07-03 13:25:57 +0800

    圖表script拉出獨立並串聯

---
+++ CHU_PolicyPlatform_1/wwwroot/js/chart_Pie.js
@@ -0,0 +1,55 @@
+﻿function chart() {
+    let categories = ["C001", "C002", "C003", "C004"];
+    let categoryLabels = {
+        "C001": "校园",
+        "C002": "教室",
+        "C003": "宿舍",
+        "C004": "餐廳"
+    };
+
+    let categoryCounts = categories.map((category) => {
+        let count = modeldata.filter((item) => item.CategoryId === category).length;
+        return count;
+    });
+
+    let categoryNames = categories.map((category) => categoryLabels[category]);
+
+    let ctx = document.getElementById('myChart').getContext('2d');
+    new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: categoryNames,
+            datasets: [{
+                backgroundColor: [
+                    '#daa547',
+                    '#8cadf0',
+                    '#dab0b1',
+                    '#547d79',
+                ],
+                borderColor: '#ffffff',
+                borderWidth: 3,
+                label: "案件量",
+                data: categoryCounts,
+                fill: false,
+            }]
+        },
+        options: {
+            plugins: {
+                legend: {
+                    labels: {
+                        color: 'white'
+                    }
+                }
+            },
+            legend: {
+                labels: {
+                    fontColor: 'white'
+                }
+            }
+        }
+    });
+}
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    chart();
+});
