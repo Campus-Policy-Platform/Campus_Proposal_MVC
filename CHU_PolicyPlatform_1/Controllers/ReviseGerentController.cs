93d93b86dbda3e5f39422e5bc2a151f90604bc2f
樂門樂曼 <diana900710@gmail.com>
2023-07-12 18:43:46 +0800

    Revert "修正錯誤"
    This reverts commit b68201f61fe7628f30f9ae5c59c77f0c38cf91d0.

--- CHU_PolicyPlatform_1/Controllers/ReviseGerentController.cs
+++ CHU_PolicyPlatform_1/Controllers/ReviseGerentController.cs
@@ -105,5 +105,65 @@ namespace CHU_PolicyPlatform_1.Controllers
 
             return RedirectToAction("AdG", "ReviseGerent");
         }
+        [HttpPost]
+        public IActionResult AdG(IFormFile csvFile)
+        {
+
+            if (csvFile == null || csvFile.Length == 0)
+            {
+                TempData["ErrorMessage"] = "你未選擇檔案。";
+                return RedirectToAction("AdG", "ReviseGerent");
+            }
+
+            string fileExtension = Path.GetExtension(csvFile.FileName);
+            if (fileExtension != ".csv")
+            {
+                TempData["ErrorMessage"] = "只接受 CSV 檔案。";
+                return RedirectToAction("AdG", "ReviseGerent");
+            }
+
+
+            using (StreamReader reader = new StreamReader(csvFile.OpenReadStream()))
+            {
+                reader.ReadLine();
+
+                List<string> errorMessages = new List<string>();
+
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    string[] fields = line.Split(',');
+
+                    string userId = fields[0];
+                    string upassword = fields[1];
+
+
+                    if (_context.Users.Any(u => u.UserId == userId))
+                    {
+                        errorMessages.Add($"使用者 {userId} 已存在，載入已跳過。");
+                        continue;
+                    }
+
+
+                    User user = new User
+                    {
+                        UserId = userId,
+                        Upassword = upassword
+                    };
+
+                    _context.Users.Add(user);
+                }
+
+                _context.SaveChanges();
+
+                if (errorMessages.Count > 0)
+                {
+                    TempData["ErrorMessages"] = errorMessages;
+                }
+            }
+
+            TempData["SuccessMessage"] = "新增成功";
+            return RedirectToAction("AdG", "ReviseGerent");
+        }
     }
 }
